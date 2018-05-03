c99a883e3ae6de85dd0d03747191161ac7a0eac6
Nnamdi Kalu <nnamdi.kalu@gmail.com>
2018-05-02 21:18:32 -0400

    Added ability to do basic math

--- parsing.c
+++ parsing.c
@@ -13,6 +13,36 @@
 
 #include "mpc.h"
 
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0) {return x + y;}
+    if (strcmp(op, "-") == 0) {return x - y;}
+    if (strcmp(op, "*") == 0) {return x * y;}
+    if (strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t){
+
+    /* return numbers directly */
+    if (strstr(t->tag, "number")){
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    /* iterate through remaining children */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expression")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+
+}
+
 int main(int argc, char** argv) {
     
     /* initial parsers */
@@ -23,16 +53,16 @@ int main(int argc, char** argv) {
     
     /* Defined as follows */
     mpca_lang(MPCA_LANG_DEFAULT,
-              "                                                             \
-              number : /-?[0-9]+/ ;                                         \
-              operator : '+' | '-' | '*' | '/';                             \
-              expression: <number> | '(' <operator>  <expression>+ ')';    \
-              phrase: /^/ <operator> <expression>+ /$/;                     \
+              "                                                                 \
+              number    : /-?[0-9]+/ ;                                          \
+              operator  : '+' | '-' | '*' | '/';                                \
+              expression: <number> | '(' <operator>  <expression>+ ')';         \
+              phrase    : /^/ <operator> <expression>+ /$/;                     \
               ",
               Number, Operator, Expression, Phrase);
     
     /* Print Version and Exit info */
-    puts("NnamLISP Version  0.0.0.1");
+    puts("NnamLISP Version  0.0.0.2");
     puts("Press CTRL+C to Exit \n");
     
     while(1){
@@ -45,7 +75,8 @@ int main(int argc, char** argv) {
         /* attempt to parse user input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Phrase, &r)){
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         }else{
             mpc_err_print(r.error);
